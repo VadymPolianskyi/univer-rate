7ac3b4b82fc50c36ddd7b3659bdffe9703095175
vadym <vadym.polyanski@gmail.com>
2017-11-18 19:03:24 +0200

    corrected comment service

--- src/main/java/com/piedpiper/univerrate/dao/repository/CommentRepository.java
+++ src/main/java/com/piedpiper/univerrate/dao/repository/CommentRepository.java
@@ -3,5 +3,9 @@ package com.piedpiper.univerrate.dao.repository;
 import com.piedpiper.univerrate.dao.entity.CommentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<CommentEntity, String> {
+
+    List<CommentEntity> findAllByUniversityId(String universityId);
 }
--- src/main/java/com/piedpiper/univerrate/dao/service/CommentService.java
+++ src/main/java/com/piedpiper/univerrate/dao/service/CommentService.java
@@ -1,11 +1,26 @@
 package com.piedpiper.univerrate.dao.service;
 
+import com.piedpiper.univerrate.dao.entity.CommentEntity;
 import com.piedpiper.univerrate.dao.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class CommentService {
+    private final CommentRepository repository;
+
     @Autowired
-    private CommentRepository commentRepository;
+    public CommentService(CommentRepository repository) {
+        this.repository = repository;
+    }
+
+    public CommentEntity save(CommentEntity commentEntity) {
+        return repository.save(commentEntity);
+    }
+
+    public List<CommentEntity> findByUniversity(String universityId) {
+        return repository.findAllByUniversityId(universityId);
+    }
 }

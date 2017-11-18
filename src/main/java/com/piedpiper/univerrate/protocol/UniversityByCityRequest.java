97c41c4e247152a0db29d3abfd2ad3154f0afc23
vadym <vadym.polyanski@gmail.com>
2017-11-18 19:37:18 +0200

    corrected constructors

--- src/main/java/com/piedpiper/univerrate/protocol/UniversityByCityRequest.java
+++ src/main/java/com/piedpiper/univerrate/protocol/UniversityByCityRequest.java
@@ -1,8 +1,12 @@
 package com.piedpiper.univerrate.protocol;
 
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.data.domain.Pageable;
 
+@AllArgsConstructor
+@NoArgsConstructor
 public class UniversityByCityRequest extends Request{
     private String city;
     private int page;
--- src/main/java/com/piedpiper/univerrate/protocol/UniversityByCityResponse.java
+++ src/main/java/com/piedpiper/univerrate/protocol/UniversityByCityResponse.java
@@ -1,9 +1,13 @@
 package com.piedpiper.univerrate.protocol;
 
 import com.piedpiper.univerrate.protocol.dto.UniversityDto;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+@AllArgsConstructor
+@NoArgsConstructor
 public class UniversityByCityResponse extends Response{
     private List<UniversityDto> universities;
 }

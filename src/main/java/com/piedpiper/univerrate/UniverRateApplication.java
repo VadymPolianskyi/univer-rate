3660fc5637498ac5b0250d2d3680982130ee8c44
vadym <vadym.polyanski@gmail.com>
2017-11-18 13:45:31 +0200

    Initial commit

---
+++ src/main/java/com/piedpiper/univerrate/UniverRateApplication.java
@@ -0,0 +1,12 @@
+package com.piedpiper.univerrate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniverRateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniverRateApplication.class, args);
+	}
+}

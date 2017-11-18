14c321253771222d6d4ab72ba60555ba9bada4f4
vadym <vadym.polyanski@gmail.com>
2017-11-18 22:07:37 +0200

    disabled cors

---
+++ src/main/java/com/piedpiper/univerrate/configuration/ServerConfiguration.java
@@ -0,0 +1,17 @@
+package com.piedpiper.univerrate.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class ServerConfiguration extends WebMvcConfigurerAdapter{
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH")
+                .allowedOrigins("http://localhost:4200")
+        ;
+    }
+}

b04a9ca136b3b63ce6821341a07105882d7e11da
vadym <vadym.polyanski@gmail.com>
2017-11-19 04:13:31 +0200

    created constant class

---
+++ src/main/java/com/piedpiper/univerrate/service/Constant.java
@@ -0,0 +1,6 @@
+package com.piedpiper.univerrate.service;
+
+public class Constant {
+    public static final String LIKE_STRATEGY = "LIKE";
+    public static final String DISLIKE_STRATEGY = "DISLIKE";
+}

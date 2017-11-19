8d736f566fb5120fd5c3743af9fc5c142f37be80
vadym <vadym.polyanski@gmail.com>
2017-11-19 03:04:01 +0200

    created db searching of top 10 universities

--- src/main/java/com/piedpiper/univerrate/dao/repository/UniversityRepository.java
+++ src/main/java/com/piedpiper/univerrate/dao/repository/UniversityRepository.java
@@ -2,11 +2,20 @@ package com.piedpiper.univerrate.dao.repository;
 
 import com.piedpiper.univerrate.dao.entity.UniversityEntity;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 
 public interface UniversityRepository extends JpaRepository<UniversityEntity, String> {
 
     Page<UniversityEntity> findByAddressIsLike(String like, Pageable pageable);
+
+    Page<UniversityEntity> findAllAndOrderByRate(Pageable pageable);
+
+    default List<UniversityEntity> findTop10() {
+        return this.findAllAndOrderByRate(new PageRequest(0, 10)).getContent();
+    }
 }

dee5aee61c0ac9104c1f6cba7867772b903222b8
vadym <vadym.polyanski@gmail.com>
2017-11-19 09:53:57 +0200

    corrected university top searching

--- src/main/java/com/piedpiper/univerrate/dao/repository/UniversityRepository.java
+++ src/main/java/com/piedpiper/univerrate/dao/repository/UniversityRepository.java
@@ -2,8 +2,10 @@ package com.piedpiper.univerrate.dao.repository;
 
 import com.piedpiper.univerrate.dao.entity.UniversityEntity;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -11,5 +13,11 @@ import java.util.List;
 public interface UniversityRepository extends JpaRepository<UniversityEntity, String> {
 
     Page<UniversityEntity> findByAddressIsLike(String like, Pageable pageable);
-    List<UniversityEntity> findTop10ByOrderByRate();
+
+    @Query("SELECT u FROM UniversityEntity u ORDER BY u.rate DESC")
+    Page<UniversityEntity> findTop10(Pageable pageable);
+
+    default List<UniversityEntity> findTop10() {
+        return findTop10(new PageRequest(0, 10)).getContent();
+    }
 }

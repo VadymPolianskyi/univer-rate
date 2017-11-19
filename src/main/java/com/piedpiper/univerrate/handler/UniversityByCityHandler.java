d7253bb01921fd32d9252b98d625d32305162cd6
vadym <vadym.polyanski@gmail.com>
2017-11-19 02:31:53 +0200

    corrected handler according to feature

--- src/main/java/com/piedpiper/univerrate/handler/UniversityByCityHandler.java
+++ src/main/java/com/piedpiper/univerrate/handler/UniversityByCityHandler.java
@@ -5,12 +5,12 @@ import com.piedpiper.univerrate.protocol.UniversityByCityRequest;
 import com.piedpiper.univerrate.protocol.UniversityByCityResponse;
 import com.piedpiper.univerrate.protocol.dto.UniversityDto;
 import com.piedpiper.univerrate.service.Mapper;
+import com.piedpiper.univerrate.service.RateService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.toList;
 
@@ -20,11 +20,13 @@ public class UniversityByCityHandler implements Handler<UniversityByCityRequest,
 
     private final UniversityService universityService;
     private final Mapper mapper;
+    private final RateService rateService;
 
     @Autowired
-    public UniversityByCityHandler(UniversityService universityService, Mapper mapper) {
+    public UniversityByCityHandler(UniversityService universityService, Mapper mapper, RateService rateService) {
         this.universityService = universityService;
         this.mapper = mapper;
+        this.rateService = rateService;
     }
 
     @Override
@@ -32,6 +34,7 @@ public class UniversityByCityHandler implements Handler<UniversityByCityRequest,
 
         List<UniversityDto> universities = universityService.getByCity(request.getCity(), request.getPageable())
                 .stream()
+                .peek(university -> university.setRate(rateService.getRate(university)))
                 .map(mapper::mapUniversity)
                 .collect(toList());
 
--- src/main/java/com/piedpiper/univerrate/handler/UniversityDetailsHandler.java
+++ src/main/java/com/piedpiper/univerrate/handler/UniversityDetailsHandler.java
@@ -1,5 +1,6 @@
 package com.piedpiper.univerrate.handler;
 
+import com.piedpiper.univerrate.dao.entity.CommentEntity;
 import com.piedpiper.univerrate.dao.service.CommentService;
 import com.piedpiper.univerrate.dao.service.UniversityService;
 import com.piedpiper.univerrate.protocol.UniversityDetailsRequest;
@@ -7,6 +8,7 @@ import com.piedpiper.univerrate.protocol.UniversityDetailsResponse;
 import com.piedpiper.univerrate.protocol.dto.CommentDto;
 import com.piedpiper.univerrate.protocol.dto.UniversityDto;
 import com.piedpiper.univerrate.service.Mapper;
+import com.piedpiper.univerrate.service.RateService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -21,35 +23,30 @@ public class UniversityDetailsHandler implements Handler<UniversityDetailsReques
     private final UniversityService universityService;
     private final CommentService commentService;
     private final Mapper mapper;
+    private final RateService rateService;
 
     @Autowired
-    public UniversityDetailsHandler(CommentService commentService, UniversityService universityService, Mapper mapper) {
+    public UniversityDetailsHandler(CommentService commentService, UniversityService universityService, Mapper mapper, RateService rateService) {
         this.commentService = commentService;
         this.universityService = universityService;
         this.mapper = mapper;
+        this.rateService = rateService;
     }
 
     @Override
     public UniversityDetailsResponse handle(UniversityDetailsRequest request) {
 
         UniversityDto university = mapper.mapUniversity(universityService.getById(request.getId()));
-        List<CommentDto> comments = commentService.findByUniversity(university.getId())
-                .stream()
+        List<CommentEntity> commentEntities = commentService.findByUniversity(university.getId());
+
+        List<CommentDto> comments = commentEntities.stream()
                 .map(mapper::mapComment)
                 .collect(Collectors.toList());
 
-        long allRates = 0;
-        for (CommentDto comment : comments) {
-            allRates += comment.getRate();
-        }
-
-        double avgRate = 0;
-
-        if (comments.size() > 0)
-            avgRate = (double) (allRates / comments.size());
+        double avgRate = rateService.getRate(commentEntities);
+        university.setRate(avgRate);
 
         log.info("Returned details about '{}' with rate {}", university.getName(), avgRate);
-
-        return new UniversityDetailsResponse(university, comments, avgRate);
+        return new UniversityDetailsResponse(university, comments);
     }
 }
